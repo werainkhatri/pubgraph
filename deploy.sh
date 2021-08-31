8bb72319e87b79e3965c70b6a4ec3e40084a390c
Viren Khatri <werainkhatri.work@gmail.com>
2021-08-31 16:55:38 +0530

    updated proxy url, navbar UI and deploy script

--- deploy.sh
+++ deploy.sh
@@ -2,10 +2,10 @@
 rm -rf dist || exit 0;
 mkdir dist;
 npm run build
-cp ./ops/CNAME ./dist
+cp ./dist
 ( cd dist
  git init
  git add .
  git commit -m "Deployed to Github Pages"
- git push --force git@github.com:anvaka/npmgraph.an.git master:gh-pages
+ git push --force git@github.com:werainkhatri/pubgraph.git master:gh-pages
 )
--- src/scripts/config.js
+++ src/scripts/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  registryUrl: ' http://we-cors-anywhere.herokuapp.com/https://pub.dev/api/packages/',
+  registryUrl: 'https://we-cors-anywhere.herokuapp.com/https://pub.dev/api/packages/',
   // todo fix autocompleter
   autoCompleteUrl: 'https://registry.npmjs.org/-/v1/search?size=10&from=0'
 }
