e31596c0b8eb8daaf1e94eaeb0abe488026fa1e2
Viren Khatri <viren.khatri@cred.club>
2022-05-27 18:04:42 +0530

    fix: update cors link

--- src/scripts/config.js
+++ src/scripts/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  registryUrl: 'https://we-cors-anywhere.herokuapp.com/https://pub.dev/api/packages/',
+  registryUrl: 'http://13.233.126.147:8002/https://pub.dev/api/packages/',
   // TODO(werainkhatri): fix autocompleter
   autoCompleteUrl: 'https://registry.npmjs.org/-/v1/search?size=10&from=0'
 }
