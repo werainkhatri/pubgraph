8be0f412ed64fdf1b624cd08863c4dfb9b152fe2
Viren Khatri <44755140+werainkhatri@users.noreply.github.com>
2022-11-02 01:37:55 +0530

    update registry url

--- src/scripts/config.js
+++ src/scripts/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  registryUrl: 'http://13.233.126.147:8002/https://pub.dev/api/packages/',
+  registryUrl: 'http://3.110.47.106:8002/https://pub.dev/api/packages/',
   // TODO(werainkhatri): fix autocompleter
   autoCompleteUrl: 'https://registry.npmjs.org/-/v1/search?size=10&from=0'
 }
